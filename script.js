ed11dab54e828f6376dba73bada6c80f7ca7d708
victoria_mali <153543182+victoria-mali@users.noreply.github.com>
2026-03-04 18:12:09 +0000

    Add restart button to play another game

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ function playRound(computerChoice, humanChoice) {
 
     let totalScore = "Computer: " + computerScore + " Human: " + humanScore;
 
+    const container = document.querySelector(".container");
+    const text = document.querySelector(".container");
     const results = document.querySelector(".results");
     const score = document.querySelector(".score");
     results.textContent = message;
@@ -36,24 +38,35 @@ function playRound(computerChoice, humanChoice) {
 
     if ((computerScore === 5) || (humanScore === 5)) {
         if (computerScore > humanScore) {
-            results.textContent = "Game over! Computer beat you!" + ` Total score is: ${totalScore}`;
+            results.textContent = "Game over! Computer beat you!";
         } else if (computerScore < humanScore)  {
-            results.textContent = "Game over! You beat computer!" + ` Total score is: ${totalScore}`;
+            results.textContent = "Game over! You beat computer!";
         } else if (computerScore === humanScore) {
-            results.textContent = "Game over! It's a tie!" + ` Total score is: ${totalScore}`;
+            results.textContent = "Game over! It's a tie!";
         }
-    score.textContent = "";
+    
+    score.textContent = `Total score is: ${totalScore}`;
     computerScore = 0;
     humanScore = 0;
+    totalScore = "";
+    buttons.style.display = "none";
+
+    
+    const startOverBtn = document.createElement("button");
+    startOverBtn.textContent = "Restart the game";
+    text.appendChild(startOverBtn);
+
+    startOverBtn.addEventListener("click", () => {
+        results.textContent = "";
+        score.textContent = "";
+        text.removeChild(startOverBtn);
+        buttons.style.display = "block";
+    })
     }
 
-/*     console.log(computerChoice);
-    console.log(humanChoice);
-    console.log(message);
-    console.log(totalScore); */
 }
 
-
+const buttons = document.querySelector(".buttons");
 const btnRock = document.querySelector(".btn-rock");
 const btnPaper = document.querySelector(".btn-paper");
 const btnScissors = document.querySelector(".btn-scissors");
